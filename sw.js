db20ec018d5f98ea097a4d6273736cf3ceb58bf6
t3uk1 <1027600674@qq.com>
2024-11-16 22:53:00 +0800

    Site updated: 2024-11-16 22:52:58

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1729941087605";
+const VERSION = "1731768772221";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",

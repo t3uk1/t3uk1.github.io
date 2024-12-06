983c5ec0e49412de6ea9a6f2aaa509c202cc5c12
t3uk1 <1027600674@qq.com>
2024-12-06 21:13:32 +0800

    Site updated: 2024-12-06 21:13:29

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732979189774";
+const VERSION = "1733490802958";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
